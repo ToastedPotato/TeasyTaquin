ebc8a171ad6ee5ab53b9d660b14f25f666b31717
Christophe <christophe_apollon@protonmail.com>
2018-07-21 20:38:19 -0400

    image découpée correctement, formulaire fonctionnel

--- taquin.js
+++ taquin.js
@@ -17,30 +17,50 @@ function afficher(){
      var url = $('#form input[name="url"]').val();
      var nbLignes = $('#form input[name="lignes"]').val();
      var nbCols = $('#form input[name="colonnes"]').val();
-     var showNumbers = $('#form input[name="numeros"]').val();
-     
+     var showNumbers = $('#form input[name="numeros"]').is(':checked');
+                        
+     var update_properties = function(src) {
+         var img = new Image();
+         $(img).one('load', function() {
+             document.styleSheets[0].cssRules[0].style.setProperty("--item-w", ""+(img.width/nbCols)+"px");
+             document.styleSheets[0].cssRules[0].style.setProperty("--item-h", ""+(img.height/nbLignes)+"px");
+             document.styleSheets[0].cssRules[0].style.setProperty("--img-src", "url("+url+")");
+             document.styleSheets[0].cssRules[0].style.setProperty("--font-s", ""+(img.height/20)+"px");
+             if(showNumbers){
+                document.styleSheets[0].cssRules[0].style.setProperty("--visible", "visible");
+             }
+         });
+         img.src = src;
+     };
+          
      // if not valid, display message 
      if(!url || !nbLignes || !nbCols) {
        //TODO : test if valid, display message
+       alert("Valeurs invalides");
        return;
      }
      
-     // Generate table
+     //updating the CSS custom properties
+     update_properties(url);
+     
+     // Generate table and display image+numbers
      $("#table").append(document.createElement("table"));     
-     for(var i=0; i < nbLignes; i++) {
-        $("table").append(document.createElement("tr"));
-        
-     }
-     for(var j=0; j < nbCols; j++) {
-       $("tr").append(document.createElement("td"));
+     var nbItems = 1;
+     for(var i=1; i <= nbLignes; i++) {
+        var row = document.createElement("tr");
+        row.classList.add(""+i);
+        $("table").append(row);
+        for(var j=1; j <= nbCols; j++) {
+            var item = document.createElement("td");
+            item.classList.add(""+j);
+            var number = "<p>"+nbItems+"</p>";
+            $("tr."+i).append(item);
+            $("tr."+i+" > td."+j).append(number);
+            $("tr."+i+" > td."+j).attr("style", "background-position:-"+((j-1)*100)+"% -"+((i-1)*100)+"%");
+            nbItems++;
+        }   
      }
      
-     // Display image and numbers 
-     //TODO : crop image/display correct cell numbers
-     $("td").text(nbCols * nbLignes).css("background-color", "yellow");
-     $("td").append(document.createElement("img"));
-     $("img").attr("src", url).attr("alt", "my image");
-
      $("label").css("background-color", "green"); //temp test
 };
 
