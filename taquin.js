3a3c8f78f665a397417b4cd5ad4bd6caa998c591
Christophe <christophe_apollon@protonmail.com>
2018-07-22 23:57:10 -0400

    shuffle works, old table removed upon displaying a new one

--- taquin.js
+++ taquin.js
@@ -41,16 +41,23 @@ function afficher(){
      }
      
      //updating the CSS custom properties
-     update_properties(url);
+     update_properties(url);     
      
-     // Generate table and display image+numbers
+     //Remove old table (if applicable), generate table and display img+numbers
+     if($("table").length > 0){$("table").remove();}
+     
      $("#table").append(document.createElement("table"));     
+     
      var nbItems = 1;
+     
      for(var i=1; i <= nbLignes; i++) {
+        
         var row = document.createElement("tr");
         row.classList.add(""+i);
         $("table").append(row);
+        
         for(var j=1; j <= nbCols; j++) {
+            
             var item = document.createElement("td");
             var number = "<p>"+nbItems+"</p>";
             item.id = ""+nbItems;
@@ -58,9 +65,7 @@ function afficher(){
             $("td#"+nbItems).append(number);
             $("td#"+nbItems).attr("style", "background-position:-"+
                 ((j-1)*100)+"% -"+((i-1)*100)+"%");
-            $("td#"+nbItems).click(function() {
-                return deplacer($(this));
-            });
+            
             if(nbItems == nbLignes*nbCols){
                 $("td#"+nbItems).addClass("last");
             }
@@ -68,17 +73,47 @@ function afficher(){
         }   
      }
      
-     //attaching arrow key listener to document
-     $(document).keyup(function(e){
-        return deplacerFleche(e.which);
-     });
 };
 
-// Mélange les tuiles du jeu existant
+//Mélange les tuiles du jeu existant
 function brasser(){
-     //TODO : shuffle tiles numbers
-     // update display
-     $("label").css("background-color", "red"); //temp test
+    //TODO : Find an algorithm that involes less nested loops?
+    var nbTiles = $("td").length;
+    var nbRows = $("tr").length;
+    var nbCols = nbTiles/nbRows;
+    var usedIds = [];
+     
+    for(var i=1; i <= nbRows; i++){
+        for (var j=0; j < nbCols; j++){
+            
+            var tileId = 0;
+           
+            //find a new random tile that isn't the last one
+            do{
+                tileId = Math.floor((Math.random()*(nbTiles-1))+1);
+            }while($.inArray(tileId, usedIds) != -1);
+            
+            //add the tile to table and its id to the list of used tiles
+            $("tr."+i).append($("td#"+tileId));
+            usedIds.push(tileId);
+            
+            //to leave an empty space for final tile
+            if(i == nbRows && j == nbCols-2){j++}
+        }
+    }
+     
+    $("tr."+nbRows).append($("td#"+nbTiles));
+     
+    //click listener added to the shuffled tiles to permit play
+    $("td").click(function() {
+        return deplacer($(this));
+    });
+    
+    //arrow key listener added to document
+    $(document).keyup(function(e){
+        return deplacerFleche(e.which);
+    });
+     
 };
 
 // Échange une tuile avec l'espace vide si celui-ci lui est adjacent
